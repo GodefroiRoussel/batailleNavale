1d11644b5132d32651aa4a0697f005aef632d55c
Clément Roig <clm.roig@gmail.com>
2016-12-13 10:51:44 +0100

    MaJ noSpace

--- implementation2_Roig-Roussel/grille.py
+++ implementation2_Roig-Roussel/grille.py
@@ -121,6 +121,9 @@ class Grille :
 	#après analyse de la direction du bateau si il y a déjà des cases ajoutées pour le numBat donné en paramètre
 	#Post-conditions: bool
 		res = True
+		placeVertical = False
+		placeHorizontal = False
+
 		if(verificationCoordonnees(x,y)):
 			# 1ère position à placer
 			if(not estValide(numBat,x,y)):
@@ -128,32 +131,28 @@ class Grille :
 				# Vérification horizontale
 				cHorizontalPos = 0
 				cHorizontalNeg = 0
-				placeHorizontal = True
 				while(verificationCoordonnees(x+cHorizontalPos,y)):
 					cHorizontalPos += 1
 				# Sortie : on rencontre une case soit prise par un bateau soit hors grille (sens positif horizontal)
 				while(verificationCoordonnees(x-cHorizontalNeg,y)):
 					cHorizontalNeg += 1
-				# Sortie : on rencontre une case soit prise par un bateau soit hors grille (sens positif horizontal)
-				placeHorizontal = (cHorizontalNeg + cHorizontalPos + 1 >= taille)
+				# Sortie : on rencontre une case soit prise par un bateau soit hors grille (sens négatif horizontal)
+				placeHorizontal = ((cHorizontalNeg + cHorizontalPos + 1) >= taille)
 
 				# Vérification verticale
 				cVerticalPos = 0
 				cVerticalNeg = 0
-				placeVertical = True
 				while(verificationCoordonnees(x+cVerticalPos,y)):
 					cVerticalPos += 1
-				# Sortie : on rencontre une case soit prise par un bateau soit hors grille (sens positif horizontal)
+				# Sortie : on rencontre une case soit prise par un bateau soit hors grille (sens positif vertical)
 				while(verificationCoordonnees(x+cVerticalNeg,y)):
 					cVerticalNeg += 1
-				# Sortie : on rencontre une case soit prise par un bateau soit hors grille (sens positif horizontal)
-				placeVertical = (cVerticalNeg + cVerticalPos +1 >= taille)
+				# Sortie : on rencontre une case soit prise par un bateau soit hors grille (sens négatif vertical)
+				placeVertical = ((cVerticalNeg + cVerticalPos +1) >= taille)
 
-			# 2ème et + position à placer
+			# 2ème ou + position à placer
 			else:
-		else:
-			placeVertical = False
-			placeHorizontal = False
+
 		return placeVertical or placeHorizontal
 
 	def estVide(self):
